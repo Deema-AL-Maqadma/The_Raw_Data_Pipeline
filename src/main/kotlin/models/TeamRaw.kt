7894856587ca62760c0bd593952125b2bf22f9fa
ALBatool8 <tot28820041@gmail.com>
2025-11-21 15:25:15 +0200

    Add teamraw data class

--- src/main/kotlin/models/TeamRaw.kt
+++ src/main/kotlin/models/TeamRaw.kt
@@ -1,2 +1,13 @@
 package models
+/**
+ * Data class representing raw team data from CSV
+ * @property teamId Unique identifier for the team
+ * @property teamName Name of the team
+ * @property mentorLead Lead mentor for the team
+ */
+data class TeamRaw(
+    val teamId: String,
+    val teamName: String,
+    val mentorLead: String
+)
 
