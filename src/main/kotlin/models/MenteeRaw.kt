c7795b2824782f8673be08d8458d2040ba8edaea
ALBatool8 <tot28820041@gmail.com>
2025-11-21 15:26:31 +0200

    Add teamraw data class

--- src/main/kotlin/models/MenteeRaw.kt
+++ src/main/kotlin/models/MenteeRaw.kt
@@ -1,5 +1,13 @@
 package models
-class MenteeRaw {
-
-}
+/**
+ * Data class representing raw mentee data from CSV
+ * @property menteeId Unique identifier for the mentee
+ * @property name Name of the mentee
+ * @property teamId Team identifier the mentee belongs to
+ */
+data class MenteeRaw(
+    val menteeId: String,
+    val name: String,
+    val teamId: String
+)
 
